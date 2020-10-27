b8ccf5fb454a599c80f9976e9ea39cc92dd8d481
pociust <poci14@gmail.com>
2020-10-26 21:24:38 -0500

    deleted default vue and hello world to start project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    Header
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
-  components: { HelloWorld },
+  components: { Header },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  div
+    | hello world
+</template>
+
+<style lang="sass" scoped>
+  div
+    background-color: red
+</style>
