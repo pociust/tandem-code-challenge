5b3751e1fa5e08f051cb49e95654fadec87ff21e
pociust <poci14@gmail.com>
2020-10-27 20:28:48 -0500

    added index to capture index of each question in the array and aded a mehod to increase the index by 1 to increment the questions, basic display of content of each object question

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,18 +1,28 @@
 <template lang="pug">
   div
-    div(v-for="question in questionArray")
-      | {{ question }}
+    div(v-for="(question, index) in questionArray")
+      div(v-if="index === numberOfQuestions")
+        | {{ question.question }}
+        div
+          | {{ question.incorrect }} {{ question.correct }}
+    button(type="button" @click="changeQuestion")
+      | test
 </template>
 
 <script>
 export default {
-//   data() {
-//     return { listOfQuestions: questionArray };
-//   },
+  data() {
+    return { numberOfQuestions: 0 };
+  },
   props: {
     questionArray: {
       type: Array,
       default: null,
+    },
+  },
+  methods: {
+    changeQuestion(number) {
+      this.numberOfQuestions += 1;
     },
   },
 };
