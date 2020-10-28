d4599b0f1727ccf06c390b82422a1401a9ee02ec
pociust <poci14@gmail.com>
2020-10-27 22:40:27 -0500

    added basic styling for question card and removed boilerplate css from app vue

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,5 +1,12 @@
+<style lang="sass" scoped>
+  .question-card
+    border: 1px solid black
+    height: 150px
+    width: 300px
+</style>
+
 <template lang="pug">
-  div
+  div(class="question-card")
     div
       | {{ question.question }}
     span(v-for="choice in question.choices")
