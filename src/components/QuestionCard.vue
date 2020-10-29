eb200aa7f6e59e923b96abcb93c828d6d3b1c874
pociust <poci14@gmail.com>
2020-10-28 21:00:27 -0500

    added styling with flexbox and inital stlying for answer selection for choices and question title

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,16 +1,45 @@
 <style lang="sass" scoped>
   .question-card
+    display: flex
+    flex-direction: column
+    justify-content: space-around
+    align-content: space-around
     border: 1px solid black
-    height: 150px
-    width: 300px
+    height: 500px
+    width: 500px
+
+  .question-title
+    font-size: 35px
+    margin: 5px
+    text-align: center
+
+  .answer-flex
+    display: flex
+    justify-content: space-around
+
+  .answer-choices
+    border: 1px solid blue
+    border-radius: 10px
+    padding: 5px
+
+  .answer-choices:hover
+      background-color: blue
+      color: white
+      padding: 5px
+      cursor: pointer
 </style>
 
 <template lang="pug">
-  div(class="question-card")
-    div
+  .question-card
+    .question-title
       | {{ question.question }}
-    div(v-for="choice in randomizedChoices" @click="selectedAnswer(choice)")
-      | {{ choice }}
+    .answer-flex
+      div(
+        class="answer-choices"
+        v-for="choice in randomizedChoices"
+        @click="selectedAnswer(choice)"
+      )
+        | {{ choice }}
 </template>
 
 <script>
