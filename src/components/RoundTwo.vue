7ccbebe38bdcb70fbd50549c6b287192ffb85247
pociust <poci14@gmail.com>
2020-10-29 20:46:34 -0500

    added a round 2 component to display round2 starting and a button to begin round to that emits to parent element that round 2 has started

---
+++ src/components/RoundTwo.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div
+    div
+      | Round 2 Fight
+    button(@click="beginRoundTwo")
+      | Start Round 2
+</template>
+<script>
+export default {
+  methods: {
+    beginRoundTwo() {
+      this.$emit('start-round-two');
+    },
+  },
+};
+</script>
