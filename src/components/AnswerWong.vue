fb59e64a61dfd7722437c465143a8670c02e46df
pociust <poci14@gmail.com>
2020-10-31 12:31:44 -0500

    added styling for displaying user wrong answer and correct answer with flexbox

--- src/components/AnswerWong.vue
+++ src/components/AnswerWong.vue
@@ -1,5 +1,16 @@
-<template lang="pug">
+<style lang="sass" scoped>
   div
+    font-size: 40px
+    text-align: center
+
+  .answer-wrong-flex
+    display: flex
+    height: 100%
+    flex-direction: column
+    justify-content: space-evenly
+</style>
+<template lang="pug">
+  div.answer-wrong-flex
     div
       | I am sorry, #[span(style="color: #e86f6d") {{ answer.answer }}] is wrong!
     div
