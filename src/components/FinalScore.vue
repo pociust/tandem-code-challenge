686e4d78026627cec1cdb5ff9cdc7383de657648
pociust <poci14@gmail.com>
2020-10-31 14:38:52 -0500

    added router-link to go home or button to submit score with input field to enter username with styling

--- src/components/FinalScore.vue
+++ src/components/FinalScore.vue
@@ -1,6 +1,51 @@
+<style lang="sass" scoped>
+  .final-score-flex
+    height: 100%
+    display: flex
+    flex-direction: column
+    justify-content: space-evenly
+
+  .score
+    color: #0ABAB5
+    border: 1px solid #0ABAB5
+    border-radius: 10px
+    padding: 5px
+    margin-left: 5px
+
+  .button-flex
+    display: flex
+    flex-direction: row
+    justify-content: space-evenly
+
+    button
+      border: 2px solid #0a68ba
+      border-radius: 5px
+      background-color: white
+      padding: 10px
+      margin: 5px
+    button:hover
+      background-color: #0a68ba
+      cursor: pointer
+      color: white
+
+    input
+      width: 100px
+      height: 33px
+      border: 2px solid #0a68ba
+      border-radius: 5px
+</style>
+
 <template lang="pug">
-  div
-    | your final score is {{ score }}
+  .final-score-flex
+    div(style="font-size: 40px")
+      | Your final score is: #[span(class="score") {{ score }}]
+    .button-flex
+      router-link(tag="button" to="/")
+        | Home
+      form
+        input(type="text" placeholder="Username")
+        button
+          | Submit score
 </template>
 
 <script>
