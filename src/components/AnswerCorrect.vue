143e42b1a7c7bf01c776025e9edd1aca9d644fdb
pociust <poci14@gmail.com>
2020-10-31 12:19:50 -0500

    increased size and aligned text for displaying correct answers

--- src/components/AnswerCorrect.vue
+++ src/components/AnswerCorrect.vue
@@ -1,3 +1,10 @@
+<style lang="sass" scoped>
+  div
+    font-size: 40px
+    text-align: center
+
+</style>
+
 <template lang="pug">
   div
     | #[span(style="color: #0ABAB5") {{ answer.answer }}] is correct!
