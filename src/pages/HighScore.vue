5f22e84b022e94f49ea0aca2cd26084c1e7117c0
pociust <poci14@gmail.com>
2020-10-31 15:57:49 -0500

    pased high scores so array of scores will be displayed with styling

--- src/pages/HighScore.vue
+++ src/pages/HighScore.vue
@@ -1,6 +1,30 @@
+<style lang="sass" scoped>
+  .title
+    font-size: 50px
+  .score-list
+    font-size: 25px
+
+    div:first-child
+      color: #0ABAB5
+
+    div:last-child
+      color: #e86f6d
+
+  .container
+    transition: border 1s
+    border: 50px solid #0a68ba
+    border-radius: 20px
+    padding: 50px
+
+</style>
+
 <template lang="pug">
-  div
-    | hello High Score {{ highScores }}
+  div(style="text-align: center" class="container")
+    .title
+      | High Scores!
+    div(class="score-list")
+      div(v-for="scores in highScores")
+        | {{ scores.userName }} : {{ scores.totalScore}}
 
 </template>
 <script>
@@ -9,7 +33,7 @@ export default {
     return { highScores: [] };
   },
   created() {
-    this.highScores = localStorage.getItem('score');
+    this.highScores = JSON.parse(localStorage.getItem('score'));
   },
 };
 </script>
