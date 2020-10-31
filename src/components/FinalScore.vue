5529009dae6ec98843cd9acdfc138fe3d0d038b3
pociust <poci14@gmail.com>
2020-10-30 19:31:10 -0500

    added final score component to display score

---
+++ src/components/FinalScore.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  div
+    | your final score is {{ score }}
+</template>
+
+<script>
+export default {
+  props: {
+    score: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
